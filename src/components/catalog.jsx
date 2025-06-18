65ab3e8ac6ad180cf75acf583ad436e63e99de26
Wilbert-Baker <wilbert.baker@sdgku.edu>
2025-06-18 18:51:52 -0400

    107 session2-4

---
+++ src/components/catalog.jsx
@@ -0,0 +1,37 @@
+//use h1 and provide a className "catalog-page"
+import product from "../components";
+import "./catalog.css";
+
+
+function Catalog()  {
+    return(
+        <div className="catalog-page">
+            <h1>Catalog Page</h1>
+            <product/>
+            <Product/>
+            <Product/>
+            <Product/>
+
+        </div>
+
+        
+    );
+            
+}
+
+            export default Catalog;
+            
+
+            
+
+           
+
+           
+           
+           
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
