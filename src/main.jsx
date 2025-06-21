4c16ed56544c301d6f30320f5f83f041b9746a75
Wilbert-Baker <wilbert.baker@sdgku.edu>
2025-06-21 12:21:09 -0400

    107 "58" session 2-5

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { strictmode}from `react`
 import {createroot} from `react-dom/client`
-import (`./index.css`)
-import { App } from './App.jsx'
+import `./index.css`
+import  App  from './App.jsx'
 
 createRoot(document.getElementById('app')).render(
     <StrictMode>
