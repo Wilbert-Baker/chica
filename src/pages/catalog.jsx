021d7b2cb356450c65cd2a13f1684fe53336bae6
Wilbert-Baker <wilbert.baker@sdgku.edu>
2025-06-22 18:07:42 -0400

    107"58"session 2-7

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -1,18 +1,33 @@
+//please create this component, just render a message "Catalog Page"
 //use h1 and provide a className "catalog-page"
-
+//please try this but in the the products
+import { useState, useEffect } from "react";
 import Product from "../components/product";
 import "./catalog.css";
+import DataService from "../service/data";
 
 function Catalog() {
-    return (
+    const [items, setItems] = useState([]);
+
+    useEffect(function(){
+        console.log("component loaded");
+        loadCatalog();
+    },[]);
+
+    function loadCatalog(){
+        //get the products 
+        let service = new DataService;
+        let productEnd = service.getProducts();
+        console.log(productEnd);
+        setItems(productEnd);
+        console.log(items);
+    }
+
+    return(
         <div className="catalog-page">
             <h1>Catalog Page</h1>
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            <h5>Hello we have {items.length} new products</h5>   
+            {/* render products according to the amouth of items that you have */}
         </div>
     );
 }
-
-export default Catalog;
